343030e35621b85e9d5717576a84cd01a9ac9ac1
lihongGZAS <411813796@qq.com>
2018-07-04 23:30:59 +0800

    购物车页面样式修改

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -13,8 +13,8 @@ Page({
       {name: 'jack', id: 11235}
     ],
     checkboxItems: [
-      { name: 'standard is dealt for u.', value: '0', checked: true}
-    ],
+      { name: 'standard is dealt for u.', value: '0', checked: false }
+    ]
   },
 
   /**
@@ -90,6 +90,25 @@ Page({
    */
   onShareAppMessage: function () {
     
+  },
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
+
+    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
+    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+      checkboxItems[i].checked = false;
+
+      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (checkboxItems[i].value == values[j]) {
+          checkboxItems[i].checked = true;
+          break;
+        }
+      }
+    }
+
+    this.setData({
+      checkboxItems: checkboxItems
+    });
   },
   goToList: function () {
     wx.switchTab({
