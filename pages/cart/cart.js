c4da17f11d5d56d36e4871eb321e9172cad8783a
lihongGZAS <411813796@qq.com>
2018-07-05 17:57:53 +0800

    修改购物车列表样式

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -10,10 +10,11 @@ Page({
    */
   data: {
     cartAry: [
-      {name: 'jack', id: 11235}
+      { name: 'DG冰雪皇后冰淇淋蛋糕', id: 11235, price: 168, imgUrl: '../../images/tianp1.jpg', goodsNum: 1, value: '0', checked: false},
+      { name: '蓝色暴风雪', id: 11342, price: 36, imgUrl: '../../images/tianp2.jpg', goodsNum: 1, value: '1', checked: true }
     ],
     checkboxItems: [
-      { name: 'standard is dealt for u.', value: '0', checked: false }
+      {value: '0', checked: false }
     ]
   },
 
@@ -25,22 +26,22 @@ Page({
     let _this = this;
     // console.log(_this.data.addressAry.length)
     // 请求服务器数据
-    // wx.request({
-    //   url: 'http://192.168.1.199', // 请求地址
-    //   header: {
-    //     'content-type': 'application/json' // 默认值
-    //   },
-    //   method: 'GET', // 默认值为GET
-    //   success: function (res) {
-    //     console.log(res.data) // 服务器返回的数据
-    //   },
-    //   fail: function (errMsg) {
-    //     wx.showModal({
-    //       title: '请求数据失败',
-    //       content: '服务器正忙，请稍后重试',
-    //     })
-    //   } 
-    // })
+    wx.request({
+      url: 'https://www.louiskane.com/set.php', // 请求地址
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      method: 'GET', // 默认值为GET
+      success: function (res) {
+        console.log(res) // 服务器返回的数据
+      },
+      fail: function (errMsg) {
+        wx.showModal({
+          title: '请求数据失败',
+          content: '服务器正忙，请稍后重试',
+        })
+      } 
+    })
   },
 
   /**
@@ -93,7 +94,45 @@ Page({
   },
   checkboxChange: function (e) {
     console.log('checkbox发生change事件，携带value值为：', e.detail.value);
+    var checkboxItems = this.data.cartAry, values = e.detail.value;
+    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+      checkboxItems[i].checked = false;
 
+      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (checkboxItems[i].value == values[j]) {
+          checkboxItems[i].checked = true;
+          break;
+        }
+      }
+    }
+
+    this.setData({
+      cartAry: checkboxItems
+    });
+
+    // 如果勾选的选项数量刚好为数据的总长度，则让结算栏的全选按钮打钩
+    if (e.detail.value.length === this.data.cartAry.length) {
+      var checkboxItems = this.data.checkboxItems;
+      for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+        checkboxItems[i].checked = true;
+      }
+
+      this.setData({
+        checkboxItems: checkboxItems
+      });   
+    } else {
+      var checkboxItems = this.data.checkboxItems;
+      for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+        checkboxItems[i].checked = false;
+      }
+
+      this.setData({
+        checkboxItems: checkboxItems
+      });
+    }
+  },
+  checkAllChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
     var checkboxItems = this.data.checkboxItems, values = e.detail.value;
     for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
       checkboxItems[i].checked = false;
@@ -109,6 +148,22 @@ Page({
     this.setData({
       checkboxItems: checkboxItems
     });
+  },
+  reduceNum: function (e) {
+    
+    var index = e.currentTarget.dataset['index'];
+    console.log(index);
+    // var cartAry = this.data.cartAry;
+    // for (var i = 0; i<cartAry.lenght; i++) {
+    //   if(index === cartAry[i])
+    // }
+    // this.data.setData({
+    //   // cartAry.goodsNum:
+    // })
+  },
+  addNum: function (e) {
+    var index = e.currentTarget.dataset['index'];
+    console.log(index);
   },
   goToList: function () {
     wx.switchTab({
